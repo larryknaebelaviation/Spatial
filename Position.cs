515213e2d6afa260b1d4b514775196106812ca9b
Larry Knaebel <lknaebel@comcast.net>
2023-06-11 23:15:34 -0400

    first commit

---
+++ Position.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Dynamic;
+using System.Runtime.InteropServices;
+
+namespace spatial
+{
+    public class Position
+    {
+        private double latitude = 0d;
+        private double longitude = 0d;
+        private double altitude = 0d;
+
+        public Position(double latitude, double longitude, double altitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.altitude = altitude;
+        }
+
+
+        public double getLatitude() => latitude;
+
+        public double getLongitude() => longitude;
+
+        public double getAltitude() => altitude;
+
+        public void setAltitude(double alt) => altitude = alt;
+
+        public void setLatitude(double lat) => latitude = lat;
+
+        public void setLongitude(double lon) => longitude = lon;
+
+        override public string ToString()
+        {
+            return $" lat={latitude} lon={longitude} alt={altitude} ";
+        }
+    }
+}
---
+++ VVector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace spatial
+{
+    public class VVector
+    {
+        private double speed;
+        private double bearing;
+        private double altitude;
+        private DateTime datetime;
+
+        public VVector(double speedknots, double bearingdegrees, DateTime datetime)
+        {
+            this.speed = speedknots;
+            this.bearing = bearingdegrees;
+            this.datetime = datetime;
+            this.altitude = -1;
+        }
+
+        public VVector(double speedknots, double bearingdegrees, double altitude, DateTime datetime)
+        {
+            this.speed = speedknots;
+            this.bearing = bearingdegrees;
+            this.datetime = datetime;
+            this.altitude = altitude;
+        }
+
+        public override string ToString()
+        {
+            return " SPEED_KT:" + speed + " HEADING:" + bearing + " ALT:" + altitude + " TIME:" + datetime.ToString(); 
+        }
+        public double getSpeed() => speed;
+
+        public double getBearing() => bearing;
+
+        public DateTime getTime() => datetime;
+
+        public double getAltitude() => altitude;
+    }
+}
